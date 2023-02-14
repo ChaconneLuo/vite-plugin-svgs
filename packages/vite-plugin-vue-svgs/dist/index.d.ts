007d9b794b5cdd0cc2cbba8a06bf7432f20679e7
ChaconneLuo <asgardannila@gmail.com>
2023-02-14 15:29:29 +0800

    clear dist

--- packages/vite-plugin-vue-svgs/dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { Plugin } from 'vite';
-
-type PluginOptions = {
-    defaultImport: string;
-};
-
-declare function createSvgIconsPlugin(options: PluginOptions): Plugin;
-
-export { PluginOptions, createSvgIconsPlugin };
